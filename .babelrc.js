a99a78216d61aeb9e7e130dbb8263ea51449f9e9
Levron Jérémy <jeremylevron@19h47.fr>
2020-02-25 11:13:29 +0100

    :wrench: .babelrc update

--- .babelrc.js
+++ .babelrc.js
@@ -18,7 +18,6 @@ const plugins = [
 	'@babel/plugin-syntax-dynamic-import',
 	'@babel/plugin-transform-runtime',
 	'@babel/plugin-transform-parameters',
-	'dynamic-import-node',
 ];
 
 module.exports = { presets, plugins };
